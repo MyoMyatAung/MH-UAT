be775101dc8a205954b97d2c8aaa53d5f2c32324
ryanreynold21 <aunkyawin2035@gmail.com>
2024-10-01 11:41:37 +0700

    fix

---
+++ src/components/login/Captch.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface CaptchProps {
+  
+}
+
+const Captch: React.FC<CaptchProps> = ({}) => {
+  return (
+    <div>
+     Captch
+    </div>
+  );
+};
+
+export default Captch;
