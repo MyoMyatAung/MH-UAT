8479ea9b57d993b60a760735f59e9e6b83f70ab4
minthiha26 <minthiha.flar@gmail.com>
2024-10-12 20:01:18 +0630

    fix bug

--- src/pages/player/video/RecommendedList.tsx
+++ src/pages/player/video/RecommendedList.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import MovieCard from "../../../components/home/MovieCard";
 
 const RecommendedList = ({ data }: any) => {
-  console.log(data?.recommendList, "rdrd");
+  // console.log(data?.recommendList, "rdrd");
   return (
     <div className="pb-20 px-5">
-      <h1 className="text-white mb-3 my-5">Recommeded</h1>
+      <h1 className="text-white mb-3 my-5 text-[16px]">相关推荐</h1>
       <div className="flex justify-start items-center flex-wrap gap-5">
         {data?.recommendList?.map((movie: any) => (
           <MovieCard movie={movie} height={"200px"} />
