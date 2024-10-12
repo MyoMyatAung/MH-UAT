42747132e03dc569619273644642f0178cb72209
minthiha26 <minthiha.flar@gmail.com>
2024-10-12 19:12:58 +0630

    add list in player

---
+++ src/pages/player/video/RecommendedList.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import MovieCard from "../../../components/home/MovieCard";
+
+const RecommendedList = ({ data }: any) => {
+  console.log(data?.recommendList, "rdrd");
+  return (
+    <div className="pb-20 px-5">
+      <h1 className="text-white mb-3 my-5">Recommeded</h1>
+      <div className="flex justify-start items-center flex-wrap gap-5">
+        {data?.recommendList?.map((movie: any) => (
+          <MovieCard movie={movie} height={"200px"} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default RecommendedList;
