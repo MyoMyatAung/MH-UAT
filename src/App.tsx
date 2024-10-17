d0ac18e7d51b457373c023b3f1c71a11e421db83
ryanreynold21 <aunkyawin2035@gmail.com>
2024-10-17 18:46:58 +0700

    update_invited_member

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,8 @@ import Favorite from "./pages/profile/Favorite";
 import Loader from "./pages/search/components/Loader";
 import ErrorToast from "./pages/profile/error/ErrorToast";
 import Landing from "./components/Landing";
-import Share from "./pages/share";
+// import Menber from "./pages/share/member";
+// import Share from "./pages/share";
 
 // Lazy load the pages
 const Home = React.lazy(() => import("./pages/home"));
@@ -44,6 +45,8 @@ const Phnumber = React.lazy(() => import("./pages/profile/Phnumber"));
 const Password = React.lazy(() => import("./pages/profile/Password"));
 const Bind = React.lazy(() => import("./pages/profile/Bind"));
 const Contact = React.lazy(() => import("./pages/profile/Contact"));
+const Share = React.lazy(() =>import("./pages/share"))
+const Member = React.lazy(() =>import("./pages/share/member"))
 
 // ProtectedRoute component to handle route guarding
 // const ProtectedRoute: React.FC<{ children: JSX.Element }> = ({ children }) => {
@@ -78,7 +81,8 @@ const App: React.FC = () => {
     location.pathname.startsWith("/update_password") ||
     location.pathname.startsWith("/bind") ||
     location.pathname.startsWith("/contact") ||
-    location.pathname.startsWith("/share")
+    location.pathname.startsWith("/share") ||
+    location.pathname.startsWith("/share/member")
 
   const hideHeader = location.pathname.startsWith("/explorer");
   useEffect(() => {
@@ -155,6 +159,7 @@ const App: React.FC = () => {
                 <Route path="/bind" element={<Bind />} />
                 <Route path="/contact" element={<Contact />} />
                 <Route path="/share" element={<Share />} />
+                <Route path="/share/member" element={<Member />} />
               </Routes>
             </Suspense>
             <ErrorToast />
--- src/features/share/ShareApi.ts
+++ src/features/share/ShareApi.ts
@@ -4,6 +4,28 @@ const ShareApi = createApi({
   reducerPath: "shareApi",
   baseQuery: fetchBaseQuery({
     baseUrl: "https://cc3e497d.qdhgtch.com:2345/api",
+    prepareHeaders: (headers) => {
+      const settings = JSON.parse(
+        localStorage.getItem("movieAppSettings") || "{}"
+      );
+      // Get the auth token from localStorage
+      const storedAuth = JSON.parse(localStorage.getItem("authToken") || "{}");
+      const accessToken = storedAuth?.data?.access_token;
+
+      headers.set("Accept-Language", "en");
+
+      if (settings.filterToggle) {
+        headers.set("X-Client-Setting", JSON.stringify({ "pure-mode": 1 }));
+      } else {
+        headers.set("X-Client-Setting", JSON.stringify({ "pure-mode": 0 }));
+      }
+      // Add Authorization header if access token exists
+      if (accessToken) {
+        headers.set("Authorization", `Bearer ${accessToken}`);
+      }
+
+      return headers;
+    },
   }),
   endpoints: (builder) => ({
     getShareScan: builder.query({
@@ -15,9 +37,20 @@ const ShareApi = createApi({
         },
       }),
     }),
+    getInvitedDetails: builder.query({
+      query: ({ act, page, pageSize }) => ({
+        url: "v1/user/invite_details",
+        method: "GET",
+        params: {
+          act: act,
+          page: page,
+          pageSize: pageSize,
+        },
+      }),
+    }),
   }),
 });
 
-export const { useGetShareScanQuery } = ShareApi;
+export const { useGetShareScanQuery, useGetInvitedDetailsQuery } = ShareApi;
 
 export default ShareApi;
