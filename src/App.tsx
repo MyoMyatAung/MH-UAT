b2b78c2db33ad9de0a3ea059199fd8ade3aeee58
hlaing-dev <hlaingmin1235145@gmail.com>
2024-11-20 21:20:17 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -141,7 +141,7 @@ const App: React.FC = () => {
 
   return (
     <>
-      {panding === 0 ? (
+      {panding ? (
         <Landing />
       ) : (
         <div className="flex flex-col min-h-screen">
