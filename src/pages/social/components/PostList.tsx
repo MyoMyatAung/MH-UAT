f7112937459ba0cdbe7070ce8b70cb588b353eb7
Htetaungkyaw <htetaung200071@gmail.com>
2024-12-14 15:31:33 +0630

    fix

--- src/pages/social/components/PostList.tsx
+++ src/pages/social/components/PostList.tsx
@@ -240,11 +240,75 @@ const PostList = ({
         >
           <div className="flex justify-between items-center mb-4 px-4 pt-4">
             <div className="flex items-center ">
-              <img
-                src={post.user.avatar}
-                alt={post.user.nickname}
-                className="w-10 h-10 rounded-full mr-4"
-              />
+              {post?.user?.avatar ? (
+                <img
+                  src={post.user.avatar}
+                  alt={post.user.nickname}
+                  className="w-10 h-10 rounded-full mr-4"
+                />
+              ) : (
+                <div className="mr-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="56"
+                    height="50"
+                    viewBox="0 0 56 50"
+                    fill="none"
+                  >
+                    <g filter="url(#filter0_d_1594_11143)">
+                      <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M28.0605 0.013916C41.2937 0.013916 51.9873 10.7075 51.9873 24.0744C51.9873 31.1588 48.9129 37.575 44.1008 41.9861C40.8927 24.8764 15.2282 24.8764 12.0202 41.9861C7.07439 37.575 4 31.1588 4 24.0744C4 10.7075 14.6935 0.013916 28.0605 0.013916ZM28.0605 12.0441C32.6052 12.0441 36.348 15.7869 36.348 20.3316C36.348 24.8764 32.6052 28.6191 28.0605 28.6191C23.5157 28.6191 19.773 24.8764 19.773 20.3316C19.773 15.7869 23.5157 12.0441 28.0605 12.0441Z"
+                        fill="white"
+                        fill-opacity="0.8"
+                        shape-rendering="crispEdges"
+                      />
+                    </g>
+                    <defs>
+                      <filter
+                        id="filter0_d_1594_11143"
+                        x="0"
+                        y="0.013916"
+                        width="55.9873"
+                        height="49.9722"
+                        filterUnits="userSpaceOnUse"
+                        color-interpolation-filters="sRGB"
+                      >
+                        <feFlood
+                          flood-opacity="0"
+                          result="BackgroundImageFix"
+                        />
+                        <feColorMatrix
+                          in="SourceAlpha"
+                          type="matrix"
+                          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                          result="hardAlpha"
+                        />
+                        <feOffset dy="4" />
+                        <feGaussianBlur stdDeviation="2" />
+                        <feComposite in2="hardAlpha" operator="out" />
+                        <feColorMatrix
+                          type="matrix"
+                          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+                        />
+                        <feBlend
+                          mode="normal"
+                          in2="BackgroundImageFix"
+                          result="effect1_dropShadow_1594_11143"
+                        />
+                        <feBlend
+                          mode="normal"
+                          in="SourceGraphic"
+                          in2="effect1_dropShadow_1594_11143"
+                          result="shape"
+                        />
+                      </filter>
+                    </defs>
+                  </svg>
+                </div>
+              )}
+
               <div>
                 <div className="flex gap-2 items-center">
                   <h4 className="font-[500] text-[16px]">
