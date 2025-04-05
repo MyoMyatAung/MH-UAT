1918e5d38e2e69e4211197e3e0b0a8c45122b647
hlaing-dev <hlaingmin1235145@gmail.com>
2025-04-05 20:15:53 +0700

    update

--- src/components/home/Banner.tsx
+++ src/components/home/Banner.tsx
@@ -10,8 +10,16 @@ const Banner = ({ list }: { list: any }) => {
   const navigate = useNavigate();
 
   const handleBannerClick = (clickLink: string) => {
-    if (clickLink && (clickLink.startsWith("http") || clickLink.startsWith("https"))) {
-      window.open(clickLink, "_blank");
+    // console.log("clickLink is=>", clickLink);
+    if (clickLink && clickLink.startsWith("http")) {
+      // Create and click an anchor element for external links
+      const a = document.createElement('a');
+      a.href = clickLink;
+      a.target = "_blank";
+      a.rel = "noopener noreferrer";
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
     } else {
       navigate(`/player/${clickLink}`);
       // Handle cases where clickLink is not a full URL or handle internal links
